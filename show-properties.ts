5f8827087df82aa6dc59eb471f46a07d83b9f240
bastien <bastien.guichard@gmail.com>
2015-06-20 03:01:12 +0200

    skeleton for angular2 clean

---
+++ show-properties.ts
@@ -0,0 +1,32 @@
+/**
+ * Created by test on 20/06/2015.
+ */
+// TypeScript
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+    selector: 'display'
+})
+
+@View({
+    template: `
+        <p>My name: {{ myName }}</p>
+        <p>Friends:</p>
+        <ul>
+            <li *for="#name of names">
+                {{ name }}
+            </li>
+        </ul>
+`,
+    directives: [For]
+})
+
+class DisplayComponent {
+    myName: string;
+    names: Array<string>;
+
+    constructor() {
+        this.myName = "Alice";
+        this.names = ["Aarav", "Martín", "Shannon", "Ariana", "Kai"];
+    }
+}
